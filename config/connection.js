429fbadf3ece92b03829956ee6fffe0d7a573df4
Habib Al Qadri <habibalqadri50@gmail.com>
2023-05-06 11:51:26 +0700

    add: Penambahan body parser

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,20 @@
 const mysql = require("mysql");
+const env = require("dotenv");
+
+env.config;
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin123",
-  database: "pos_db",
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error", err);
+  }
+  console.log("connect");
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+// const db = require("./config/connection");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hello hhahs");
+});
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.send("testing");
 });
 
 app.listen(port, () => {
