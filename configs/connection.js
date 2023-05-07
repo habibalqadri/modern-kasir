2e29890720b63265c1f1b4f8756f867a70bf4a2f
Habib Al Qadri <habibalqadri50@gmail.com>
2023-05-07 08:08:52 +0700

    add: Penambahan body parser dan test post

--- configs/connection.js
+++ configs/connection.js
@@ -1,8 +1,6 @@
 const mysql = require("mysql2");
-const env = require("dotenv");
-const util = require("util");
 
-env.config();
+//konfigurasi database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -10,14 +8,11 @@ const connection = mysql.createConnection({
   database: "pos_db",
 });
 
-connection.query = util.promisify(connection.query).bind(connection);
-
 connection.connect((err) => {
   if (err) {
-    console.log("error", err);
-  } else {
-    console.log("connected");
+    console.log("err");
   }
-});
 
+  console.log("Connected");
+});
 module.exports = connection;
--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//import body parser
+const bodyParser = require("body-parser"); //digunakan untuk menerima json dari body request
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//test post
+// app.post("/tes-post", (req, res) => {
+//   console.log(req.body);
+//   res.send("berhasil ngepost");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
